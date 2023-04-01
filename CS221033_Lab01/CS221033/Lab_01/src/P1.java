752ae455bbe00adfaa883bdf41bdf27f00511dac
Ilf Ali Momin <109297627+Mominnest@users.noreply.github.com>
2023-04-01 22:20:12 +0500

    Add files via upload

---
+++ CS221033_Lab01/CS221033/Lab_01/src/P1.java
@@ -0,0 +1,22 @@
+public class P1 {
+    public static void main(String[] args) {
+
+        System.out.println("\t\t\t\t\t\t\t\t---------------------");
+        System.out.println("\t\t\t\t\t\t\t\tDHA SUFFA UNIVERSITY");
+        System.out.println("\t\t\t\t\t\t\t\t---------------------");
+        System.out.println("\t\t\t\t\t\t\t\t\t Transcript");
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println("Name = Ilf Ali Momin\t\t\t\t\t\t\t\tRoll no. = CS221033");
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println("Subject\t\t\t\t\t\t\t\t\t\t\t\t\t  Marks");
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println("Programming Fundamental\t\t\t\t\t\t\t\t\t\t90");
+        System.out.println("Programming Fundamental Lab\t\t\t\t\t\t\t\t\t93");
+        System.out.println("Applied Physics\t\t\t\t\t\t\t\t\t\t\t\t84");
+        System.out.println("English\t\t\t\t\t\t\t\t\t\t\t\t\t\t93");
+        System.out.println("Calculus\t\t\t\t\t\t\t\t\t\t\t\t\t87");
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println("Percentage =\t\t\t\t\t\t\t\t\t\t\t  90.0%");
+        System.out.println("Cgpa =\t\t\t\t\t\t\t\t\t\t\t\t\t   4.0");
+    }
+}
